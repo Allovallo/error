1b4f721a60c17bbb80ee87a509b9613d65d06105
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-10-19 18:28:36 +0300

    Update 01-error.js
    Update 01-error.js

--- src/js/01-error.js
+++ src/js/01-error.js
@@ -1,6 +1,44 @@
-try {
-    console.log('Початок блоку try');
-    console.log('Кінець блоку try');
-} catch(err) {
-    console.log('Catch ігноруєтьсяб оскільк помилки відсутні');
-}
+// try {
+//     console.log('Початок блоку try');
+//     lalala;
+//     console.log('Кінець блоку try - ніколи не виконається');
+// } catch(err) {
+//     console.log('Виникла помилка!');
+// }
+
+// try {
+//     setTimeout(function () {
+//         noSuchVar
+//     }, 1000);
+// } catch (e) {
+//     console.log("Не спрацює!!!");
+// }
+
+// setTimeout(() => {
+//     try {
+//         noSuchVar;
+//     } catch (error) {
+//         console.log('Помилка перехоплена!!!');
+//     }
+// }, 1000);
+
+// try {
+//     lalala;
+// } catch (err) {
+//     console.log(err.name);
+//     console.log(err.message);
+//     console.log(err.stack);
+//     console.log(err);
+// }
+
+// let json = "{Некоректний JSON}";
+
+// try {
+//     let user = JSON.parse(json);
+//     console.log(user.name);
+// } catch (err) {
+//     console.log('Вибачте, в даних помилка, спробуємо отримати ще раз');
+//     console.log(err.name);
+//     console.log(err.message);
+// }
+
