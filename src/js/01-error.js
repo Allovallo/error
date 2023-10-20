5472477ad465074d276f4bc0835ba334166d9ea7
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-10-20 14:46:09 +0300

    Files updated

--- src/js/01-error.js
+++ src/js/01-error.js
@@ -42,3 +42,102 @@
 //     console.log(err.message);
 // }
 
+
+// let json = '{ "age:" 30 }';
+
+// try {
+//     const user = JSON.parse(json);
+
+//     if (!user.name) {
+//         throw new SyntaxError("Помилка - відсутнє ім'я!!!");
+//     }
+
+//     console.log(user.name);
+
+// } catch(e) {
+//     console.log("JSON Error: " + e.message);
+// }
+
+
+// let json = '{"age": 30}';
+
+// console.log(json);
+
+// try {
+//     user = JSON.parse(user);
+// } catch (error) {
+//     console.log(error.name);
+// }
+
+// let json = '{"age": 30}';
+
+// try {
+//     let user = JSON.parse(json);
+
+//     if (!user.name) {
+//         throw new SyntaxError("Дані не повні - відсутнє ім'я!!!")
+//     }
+
+//     blabla();
+
+//     console.log(user.name);
+// } catch (error) {
+//     if (error.name == "SyntaxError") {
+//         console.log("JSON Error: " + error.message);
+//     } else {
+//         throw error;
+//     }
+// }
+
+
+// function readData() {
+//     let json = '{"age": 30}';
+
+//     try {
+//         blabla();
+//     } catch (error) {
+//         if (error.name != "SyntaxError") {
+//             throw error;
+//         }
+//     }
+// }
+
+// try {
+//     readData();
+// } catch (error) {
+//     console.log("Зовнішній catch спіймав " + error);
+// }
+
+// try {
+//     alert('try');
+//     if (confirm('Згенерувати помилку?')) BAD_CODE();
+// } catch (error) {
+//     alert('catch');
+// } finally {
+//     alert('finally');
+// }
+
+let num = +prompt("Введить позитивне ціле число", 35);
+
+let ditt, result;
+
+function fib (n) {
+    if (n < 0 || Math.trunc(n) != 0) {
+        throw new Error("Має бути ціле позитивне число!");
+    }
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+
+let start = Date.now();
+
+try {
+    result = fib(num);
+} catch (e) {
+    result = 0;
+} finally {
+    diff = Date.now() - start;
+}
+
+console.log(result || "Виникла помилка!");
+
+console.log(`Виконання зайняло ${diff} мс`);
