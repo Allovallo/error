1d4985e3ab46d7c32ad58d390d6700867cab8181
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-10-27 21:12:08 +0300

    Update 02-promise.js
    Update 02-promise.js

--- src/js/02-promise.js
+++ src/js/02-promise.js
@@ -506,3 +506,36 @@
 //   new Promise((resolve, reject) => setTimeout(() => reject(new Error('Very BIG ERROR!')), 2000)),
 //   new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000)),
 // ]).catch(alert);
+
+// Promise.all([
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(1), 3000);
+//   }),
+//   'true',
+//   3,
+// ]).then(alert);
+
+// let urls = [
+//   'https://api.github.com/users/iliakan',
+//   'https://api.github.com/users/remy',
+//   'https://no-such-url',
+// ];
+
+// Promise.allSettled(urls.map(url => fetch(url))).then(results => {
+//   results.forEach((result, num) => {
+//     if (result.status == 'fulfilled') {
+//       alert(`${urls[num]}: ${result.value.status}`);
+//     }
+//     if (result.status == 'rejected') {
+//       alert(`${urls[num]}: ${result.reason}`);
+//     }
+//   });
+// });
+
+// Promise.race([
+//   new Promise((resolve, reject) =>
+//     setTimeout(() => reject(new Error('OOOPS!')), 5000),
+//   ),
+//   new Promise((resolve, reject) => setTimeout(() => resolve(2), 4000)),
+//   new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000)),
+// ]).then(alert);
